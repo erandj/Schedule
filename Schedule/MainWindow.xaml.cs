24ee934e0b6c278c784a6a07e4054bd0dd43cc1e
Степанов Эллей < @309-14>
2023-10-11 11:47:05 +0900

    Добавьте файлы проекта.

---
+++ Schedule/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Schedule
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DispatcherTimer LiveTime = new DispatcherTimer();
+            LiveTime.Interval = TimeSpan.FromSeconds(1);
+            LiveTime.Tick += TimerTick;
+            LiveTime.Start();
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            LiveTimeLabel.Content = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        private void Create_Click(object sender, RoutedEventArgs e)
+        {
+            CreateSchedule window = new CreateSchedule();
+            window.Show();
+        }
+
+        private void DownloadExcel_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
