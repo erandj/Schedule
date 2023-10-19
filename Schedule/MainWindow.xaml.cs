c2fe81d9b6011543907d949d83c92d13e766cc8f
erandj <neizvest000@gmail.com>
2023-10-19 10:29:13 +0900

    1

--- Schedule/MainWindow.xaml.cs
+++ Schedule/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,7 +30,47 @@ namespace Schedule
             LiveTime.Tick += TimerTick;
             LiveTime.Start();
         }
+        
+        private void UpdateDataGrid(object sender, RoutedEventArgs e)
+        {
+            // (ScheduledRoute)Aircraft, time, terminal, gate, status; (Route)flight, destination
+            AirportEntities context = new AirportEntities();
 
+            DbSet<ScheduledRoute> scheduledRoutes = context.ScheduledRoute;
+            DbSet<Route> routes = context.Route;
+            DbSet<Airport> airports = context.Airport;
+            DbSet<ScheduledRouteStatus> statuses = context.ScheduledRouteStatus;
+
+            var query =
+                from ScheduledRoute in scheduledRoutes
+                join Route in routes on ScheduledRoute.Route equals Route
+                join Airport in airports on Route.destination_id equals Airport.id
+                join Status in statuses on ScheduledRoute.ScheduledRouteStatus equals Status
+                select new
+                {
+                    airline = Route.Airline.name,
+                    time = ScheduledRoute.scheduled_departure_time,
+                    flight = Route.flight_number_numeric,
+                    destination = Airport.name,
+                    status = Status.name,
+                    terminal = ScheduledRoute.Terminal.ToList(),
+                    gate = ScheduledRoute.Gate.ToList(),
+                };
+
+            /*
+            foreach(var a in query)
+            {
+                Console.WriteLine("{0}, {1}, {2}, {3}, {4}", a.aircraft, a.time, a.status, a.flight, a.destination);
+                foreach(var b in a.terminal)
+                {
+                    Console.WriteLine("------- {0}", b.number);
+                }
+            }
+            */
+
+
+            FlightTable.ItemsSource = query.ToList();
+        }
         private void TimerTick(object sender, EventArgs e)
         {
             LiveTimeLabel.Content = DateTime.Now.ToString("HH:mm:ss");
